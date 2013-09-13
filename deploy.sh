34baf33994714f21304387b21c89af144209f863
Jonathan Webb <webbj2012@jwwebb.info>
2013-09-12 20:15:27 -0700

    Adds library deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Determine deployment path
+#
+DEPLOY_PATH="`dirname \"$0\"`"
+DEPLOY_PATH="`( cd \"$DEPLOY_PATH\" && pwd )`"
+if [ -z "$DEPLOY_PATH" ] ; then
+  exit 1
+fi
+echo "DEPLOYING TO ${DEPLOY_PATH}"
+
+#
+# Checkout Google API PHP Client
+#
+svn checkout http://google-api-php-client.googlecode.com/svn/tags/0.6.6/ ${DEPLOY_PATH}/lib/google-api-php-client
+
